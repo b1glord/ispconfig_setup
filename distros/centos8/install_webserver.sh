e88a035cc71e4ed0677332f7bc015ba27224cfed
BigLorD <FuryTr@yandex.com>
2020-11-10 22:01:48 +0300

    Update install_webserver.sh

--- distros/centos8/install_webserver.sh
+++ distros/centos8/install_webserver.sh
@@ -8,13 +8,13 @@ InstallWebServer() {
 	CFG_NGINX=n
 	CFG_APACHE=y
   echo -n "Installing Apache and modules... "
- 	dnf -y install httpd
+ 	dnf_install httpd
 	#dnf -y install php httpd httpd-tools mod_ssl
 	sed -i '/; Note: This value is mandatory./a listen = 9000' /etc/php-fpm.d/www.conf
 
   echo -n "Installing PHP and Modules... "
-	dnf -y install   php
-	dnf -y install php-gd php-fpm php-pdo php-gmp php-dbg php-pdo php-xml php-cli php-dba php-soap php-snmp php-ldap php-pear php-intl php-json php-odbc php-devel php-pgsql php-common php-recode php-bcmath php-xmlrpc php-mysqlnd php-enchant php-process php-opcache php-mbstring php-pecl-zip php-embedded php-pecl-apcu php-pecl-apcu-devel
+	dnf_install   php
+	dnf_install php-gd php-fpm php-pdo php-gmp php-dbg php-pdo php-xml php-cli php-dba php-soap php-snmp php-ldap php-pear php-intl php-json php-odbc php-devel php-pgsql php-common php-recode php-bcmath php-xmlrpc php-mysqlnd php-enchant php-process php-opcache php-mbstring php-pecl-zip php-embedded php-pecl-apcu php-pecl-apcu-devel
   echo -e "[${green}DONE${NC}]\n"
 	sed -i "s/error_reporting = E_ALL \& ~E_DEPRECATED \& ~E_STRICT/error_reporting = E_ALL \& ~E_NOTICE \& ~E_DEPRECATED/" /etc/php.ini
 	sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=1/" /etc/php.ini
@@ -36,7 +36,7 @@ InstallWebServer() {
 	 echo -e "${green}done! ${NC}\n"
 
   echo "Installing phpMyAdmin... "
-  	yum_install https://rpms.remirepo.net/enterprise/remi-release-8.rpm
+  	dnf_install https://rpms.remirepo.net/enterprise/remi-release-8.rpm
 	yum --enablerepo=remi install -y phpmyadmin
 	echo -e "[${green}DONE${NC}]\n"
     sed -i "s/Require ip 127.0.0.1/#Require ip 127.0.0.1/" /etc/httpd/conf.d/phpMyAdmin.conf
