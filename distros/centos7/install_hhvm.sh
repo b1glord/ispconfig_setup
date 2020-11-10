0ce65795624ea8f326910d9f66aec4441b8c30cc
BigLorD <FuryTr@yandex.com>
2020-11-10 22:21:04 +0300

    Update install_hhvm.sh

--- distros/centos7/install_hhvm.sh
+++ distros/centos7/install_hhvm.sh
@@ -1,5 +1,5 @@
 InstallHHVM() {
-  if [ $CFG_HHVM = "yes" ]; then
+# if [ $CFG_HHVM = "yes" ]; then
     echo -n "Installing HHVM (Hip Hop Virtual Machine)... "
 yum_install cpp gcc-c++ cmake git psmisc {binutils,boost,jemalloc,numactl}-devel \
 {ImageMagick,sqlite,tbb,bzip2,openldap,readline,elfutils-libelf,gmp,lz4,pcre}-devel \
