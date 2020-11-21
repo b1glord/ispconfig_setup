4dfe8479ec24a220b76d8bf6e312a641d0e51bbf
BigLorD <FuryTr@yandex.com>
2020-11-21 18:21:14 +0300

    Update install_postfix.sh

--- distros/centos7/install_postfix.sh
+++ distros/centos7/install_postfix.sh
@@ -13,14 +13,17 @@ InstallPostfix() {
   mkdir /etc/mailman/
   touch /etc/mailman/virtual-mailman
   postmap /etc/mailman/virtual-mailman
-
+  if [ "$CFG_WEBSERVER" == "apache" ]; then
+	CFG_NGINX=n
+	CFG_APACHE=y
+sed -i "s/    #user =/    user = postfix/" /etc/dovecot/conf.d/10-master.conf
+sed -i "s/    #group =/    group = postfix/" /etc/dovecot/conf.d/10-master.conf
+fi
     elif [ "$CFG_WEBSERVER" == "nginx" ]; then
   	CFG_NGINX=y
 	  CFG_APACHE=n
-
-	sed -i "s/    #user =/    user = postfix/" /etc/dovecot/conf.d/10-master.conf
-  sed -i "s/    #group =/    group = postfix/" /etc/dovecot/conf.d/10-master.conf
-
+sed -i "s/    #user =/    user = postfix/" /etc/dovecot/conf.d/10-master.conf
+sed -i "s/    #group =/    group = postfix/" /etc/dovecot/conf.d/10-master.conf
 fi
 
   systemctl enable postfix.service
