0022677d72f97ab0bef4fdb56955654b0672b2f4
BigLorD <FuryTr@yandex.com>
2021-01-15 12:17:15 +0400

    Update install_varnish.sh

--- distros/centos7/install_varnish.sh
+++ distros/centos7/install_varnish.sh
@@ -1,13 +1,7 @@
 InstallVarnish() {
 if [ $CFG_VARNISH = "yes" ]; then
-  echo -n "Installing Varnish Cache... "
-    yum_install varnish
+  echo -n "Configure Webserver... "
   
-  echo -n "Configure Varnish Cache... "
-	  sed -i "s/VARNISH_LISTEN_PORT=6081/VARNISH_LISTEN_PORT=80/" /etc/varnish/varnish.params
-	  sed -i 's/    .port = "8080";/    .port = "8090";/' /etc/varnish/default.vcl
-    systemctl enable varnish.service
-
   if [ "$CFG_WEBSERVER" == "apache" ]; then
 	CFG_NGINX=n
 	CFG_APACHE=y
@@ -15,8 +9,7 @@ if [ $CFG_VARNISH = "yes" ]; then
 # Setting Up Apache Service
   systemctl stop httpd.service
   sed -i "s/Listen 80/Listen 8090/" /etc/httpd/conf/httpd.conf
-  systemctl restart httpd.service
-  systemctl start varnish.service
+  systemctl start httpd.service
 
   elif [ "$CFG_WEBSERVER" == "nginx" ]; then
   	CFG_NGINX=y
@@ -24,12 +17,19 @@ if [ $CFG_VARNISH = "yes" ]; then
 
 # Setting Up Nginx Service
   systemctl stop nginx.service
-	sed -i "s/        listen       80 default_server;/        listen       8090 default_server;/" /etc/nginx/nginx.conf
-  sed -i "s/        listen       [::]:80 default_server;/        listen       [::]:8090 default_server;/" /etc/nginx/nginx.conf
-  systemctl restart nginx.service
-  systemctl start varnish.service
-
-  echo -e "${green}done! ${NC}\n"
+   sed -i "s/        listen       80 default_server;/        listen       8090 default_server;/" /etc/nginx/nginx.conf
+   sed -i "s/        listen       [::]:80 default_server;/        listen       [::]:8090 default_server;/" /etc/nginx/nginx.conf
+  systemctl start nginx.service
   fi
+  
+  echo -n "Installing Varnish Cache... "
+    yum_install varnish
+  
+  echo -n "Configure Varnish Cache... "
+	  sed -i "s/VARNISH_LISTEN_PORT=6081/VARNISH_LISTEN_PORT=80/" /etc/varnish/varnish.params
+	  sed -i 's/    .port = "8080";/    .port = "8090";/' /etc/varnish/default.vcl
+    systemctl enable varnish.service  
+    systemctl start varnish.service
+      echo -e "${green}done! ${NC}\n"
 fi
 }
