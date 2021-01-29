d4b6bb574cfaa3a44f45dd46c6de8ed0d4451863
BigLorD <FuryTr@yandex.com>
2021-01-29 12:25:47 +0300

    Update install_mysql.sh

--- distros/centos7/install_mysql.sh
+++ distros/centos7/install_mysql.sh
@@ -4,7 +4,7 @@
 #---------------------------------------------------------------------
 InstallSQLServer() {
   echo -n "Installing Database server (MariaDB)... "
- # curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash -s -- --mariadb-server-version="mariadb-10.4.17"
+  curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash -s -- --mariadb-server-version="mariadb-10.4.10"
   yum_install mariadb-server expect
   systemctl enable mariadb.service
   systemctl start mariadb.service
